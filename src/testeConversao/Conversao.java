64deac3b9bd9a6e1374eb396599dc47d5ccf71ec
Esdrascorreia <158611774+Esdrascorreia@users.noreply.github.com>
2024-06-21 13:27:35 -0300

    Initial commit

---
+++ src/testeConversao/Conversao.java
@@ -0,0 +1,18 @@
+package testeConversao;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+
+		 double salario = 2700.50;
+		 int valor = (int) salario;
+		 System.out.println(valor);
+		 
+		 float pontoflutuante = (float) 3.14;
+		 
+		 int valor2 = 20000000;
+		 long numeroGrande = 3456797927468998L;
+         short numeropequeno = 2103;
+         byte b = 127;
+    }
+}
---
+++ src/variaveis/TestaEscopo.java
@@ -0,0 +1,33 @@
+package variaveis;
+
+public class TestaEscopo {
+
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais. ");
+		int idade = 20;
+		int quantidadePessoa = 1;
+		
+		//boolean acompanhado = quantidadePessoa >= 2;
+		boolean acompanhado;
+		
+		if(quantidadePessoa >= 2) {
+			
+			acompanhado = true;
+				
+		} else {
+			acompanhado = false;
+			
+		}
+		
+		System.out.println("Valor de acompanhado é" + acompanhado);
+		
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Você pode comprar uma bebida.");
+		} else {
+			System.out.println("Você não pode comprar bebida.");
+		}
+
+	}
+}
+
+
